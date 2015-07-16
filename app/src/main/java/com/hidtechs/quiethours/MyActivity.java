5c33596d9f14a3ad3079f73b43a856746e199ba1
Abhishek Prakash <abskpr@hotmail.com>
2015-07-16 09:42:20 +0530

    partially fixed the SetTimeActivity

--- app/src/main/java/com/hidtechs/quiethours/MyActivity.java
+++ app/src/main/java/com/hidtechs/quiethours/MyActivity.java
@@ -1,8 +1,8 @@
 package com.hidtechs.quiethours;
 
 import android.annotation.SuppressLint;
-import android.annotation.TargetApi;
 import android.app.KeyguardManager;
+import android.app.TimePickerDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -11,9 +11,7 @@ import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.media.AudioManager;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.PowerManager;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SwitchCompat;
@@ -22,90 +20,92 @@ import android.telephony.TelephonyManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.TimePicker;
+
+import java.math.BigDecimal;
 
 public class MyActivity extends AppCompatActivity implements View.OnClickListener {
 
     SwitchCompat s1, s2;
     LinearLayout timerow;
     Intent intent;
-    TextView t1,t2;
+    TextView t1, t2, ft, tt;
     SharedPreferences preferences;
     BroadcastReceiver mReceiver;
     IntentFilter filter;
     TelephonyManager telephonyManager;
     AudioManager audioManager;
-    String number;
+    String number, ftenthHour, fonceHour, ftenthMinute, fonceMinute, ttenthHour, tonceHour, ttenthMinute, tonceMinute;
     KeyguardManager keyguardManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        audioManager= (AudioManager) getSystemService(AUDIO_SERVICE);
-        telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
-        keyguardManager= (KeyguardManager)getSystemService(Context.KEYGUARD_SERVICE);
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        keyguardManager = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
         PhoneStateListener callStateListener = new PhoneStateListener() {
             @SuppressLint("NewApi")
-            public void onCallStateChanged(int state, String incomingNumber)
-            {
-                number=incomingNumber;
+            public void onCallStateChanged(int state, String incomingNumber) {
+                number = incomingNumber;
 
                 // If phone ringing
-                if(state==TelephonyManager.CALL_STATE_RINGING)
-                {
-                    if (preferences.getBoolean("allcontacts",false) && preferences.getBoolean("silentButton",false) && preferences.getBoolean("callsButton",false) && contactExists(this,number))
-                    {
+                if (state == TelephonyManager.CALL_STATE_RINGING) {
+                    if (preferences.getBoolean("allcontacts", false) && preferences.getBoolean("silentButton", false) && preferences.getBoolean("callsButton", false) && contactExists(this, number)) {
                         audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
                     }
-
                 }
                 // If incoming call received
-                if(state==TelephonyManager.CALL_STATE_OFFHOOK)
-                {
+                if (state == TelephonyManager.CALL_STATE_OFFHOOK) {
 
                 }
-
-
-                if(state==TelephonyManager.CALL_STATE_IDLE)
-                {
-                    if (preferences.getBoolean("silentButton",false) && keyguardManager.isKeyguardLocked())
-                    {
+                //Normal state
+                if (state == TelephonyManager.CALL_STATE_IDLE) {
+                    if (preferences.getBoolean("silentButton", false) && keyguardManager.isKeyguardLocked()) {
                         audioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
                     }
-
                 }
             }
         };
-        telephonyManager.listen(callStateListener,PhoneStateListener.LISTEN_CALL_STATE);
+        telephonyManager.listen(callStateListener, PhoneStateListener.LISTEN_CALL_STATE);
         filter = new IntentFilter(Intent.ACTION_SCREEN_ON);
         filter.addAction(Intent.ACTION_SCREEN_OFF);
         filter.addAction(Intent.ACTION_USER_PRESENT);
         mReceiver = new ScreenReceiver();
         registerReceiver(mReceiver, filter);
-        preferences = getSharedPreferences("MyFiles",MODE_PRIVATE);
+        preferences = getSharedPreferences("MyFiles", MODE_PRIVATE);
         boolean rbpref = preferences.getBoolean("repeatButton", false);
-        boolean sbpref = preferences.getBoolean("silentButton",false);
+        boolean sbpref = preferences.getBoolean("silentButton", false);
         setContentView(R.layout.activity_my);
+
         s1 = (SwitchCompat) findViewById(R.id.silent_switch1);
         s2 = (SwitchCompat) findViewById(R.id.repeat_switch);
-        timerow = (LinearLayout) findViewById(R.id.linearLayout3);
         t1 = (TextView) findViewById(R.id.setTime);
         t2 = (TextView) findViewById(R.id.exceptions_settings);
+        ft = (TextView) findViewById(R.id.main_from_time);
+        tt = (TextView) findViewById(R.id.main_to_time);
+        timerow = (LinearLayout) findViewById(R.id.linearLayout3);
+
         s1.setOnClickListener(this);
         s2.setOnClickListener(this);
         t1.setOnClickListener(this);
         t2.setOnClickListener(this);
+
         s1.setChecked(sbpref);
         s2.setChecked(rbpref);
+
         isRepeatOn();
 
     }
+
     public boolean contactExists(PhoneStateListener context, String number) {
         try {
             Uri uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(number));
-            Cursor cursor = getContentResolver().query(uri, new String[]{ContactsContract.PhoneLookup.DISPLAY_NAME}, number, null, null );
+            Cursor cursor = getContentResolver().query(uri, new String[]{ContactsContract.PhoneLookup.DISPLAY_NAME}, number, null, null);
             try {
                 if (cursor.moveToFirst()) {
                     return true;
@@ -120,15 +120,33 @@ public class MyActivity extends AppCompatActivity implements View.OnClickListene
         return false;
     }
 
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (!preferences.getBoolean("silentButton",false))
-        {
+        if (!preferences.getBoolean("silentButton", false)) {
             unregisterReceiver(mReceiver);
         }
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        int fromHour_x = preferences.getInt("fromHour", 22);
+        int fromMinute_x = preferences.getInt("fromMinute", 0);
+        ftenthHour = String.valueOf(fromHour_x / 10);
+        fonceHour = String.valueOf(fromHour_x % 10);
+        ftenthMinute = String.valueOf(fromMinute_x / 10);
+        fonceMinute = String.valueOf(fromMinute_x % 10);
+        ft.setText("From " + ftenthHour + "" + fonceHour + " : " + ftenthMinute + "" + fonceMinute);
+        int toHour_x = preferences.getInt("toHour", 6);
+        int toMinute_x = preferences.getInt("toMinute", 0);
+        ttenthHour = String.valueOf(toHour_x / 10);
+        tonceHour = String.valueOf(toHour_x % 10);
+        ttenthMinute = String.valueOf(toMinute_x / 10);
+        tonceMinute = String.valueOf(toMinute_x % 10);
+        tt.setText(" to " + ttenthHour + "" + tonceHour + " : " + ttenthMinute + "" + tonceMinute);
+    }
+
 
     private void isRepeatOn() {
         if (!s2.isChecked()) {
@@ -137,8 +155,6 @@ public class MyActivity extends AppCompatActivity implements View.OnClickListene
             timerow.setVisibility(timerow.VISIBLE);
         }
     }
-
-
 
 
     @Override
@@ -159,7 +175,6 @@ public class MyActivity extends AppCompatActivity implements View.OnClickListene
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -169,7 +184,6 @@ public class MyActivity extends AppCompatActivity implements View.OnClickListene
             SharedPreferences.Editor editor = preferences.edit();
             editor.putBoolean("silentButton", s1.isChecked());
             editor.commit();
-
         }
         if (v.getId() == s2.getId()) {
             SharedPreferences.Editor editor = preferences.edit();
@@ -187,12 +201,12 @@ public class MyActivity extends AppCompatActivity implements View.OnClickListene
             startActivity(intent);
         }
 
-        if(v.getId()==t2.getId())
-        {
-            intent=new Intent(this,ExceptionActivity.class);
+        if (v.getId() == t2.getId()) {
+            intent = new Intent(this, ExceptionActivity.class);
             startActivity(intent);
 
         }
     }
+
 }
 
--- app/src/main/java/com/hidtechs/quiethours/SetTimeActivity.java
+++ app/src/main/java/com/hidtechs/quiethours/SetTimeActivity.java
@@ -15,26 +15,55 @@ import android.widget.TimePicker;
 public class SetTimeActivity extends AppCompatActivity implements CheckBox.OnCheckedChangeListener {
 
     TextView fromTime, toTime, fromTimeShow, toTimeShow;
-    static final int FROMTEXT_DIALOG_ID = 0;
-    static final int TOTEXT_DIALOG_ID = 1;
-    int fromHour_x,fromMinute_x,toHour_x,toMinute_x;
+    public static final int FROMTEXT_DIALOG_ID = 0;
+    public static final int TOTEXT_DIALOG_ID = 1;
+    int fromHour_x, fromMinute_x, toHour_x, toMinute_x;
     CheckBox check_all, check_mon, check_tue, check_wed, check_thu, check_fri, check_sat, check_sun;
     SharedPreferences preferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_time);
+
         fromTimeShow = (TextView) findViewById(R.id.from_time_show);
         toTimeShow = (TextView) findViewById(R.id.to_time_show);
         check_all = (CheckBox) findViewById(R.id.always);
+        check_mon = (CheckBox) findViewById(R.id.day_mon);
+        check_tue = (CheckBox) findViewById(R.id.day_tue);
+        check_wed = (CheckBox) findViewById(R.id.day_wed);
+        check_thu = (CheckBox) findViewById(R.id.day_thu);
+        check_fri = (CheckBox) findViewById(R.id.day_fri);
+        check_sat = (CheckBox) findViewById(R.id.day_sat);
+        check_sun = (CheckBox) findViewById(R.id.day_sun);
+
         check_all.setOnCheckedChangeListener(this);
-        preferences=getSharedPreferences("MyFiles",MODE_PRIVATE);
-        fromHour_x=preferences.getInt("fromHour", 22);
-        fromMinute_x=preferences.getInt("fromMinute", 00);
-        fromTimeShow.setText(fromHour_x+" : "+fromMinute_x);
-        toHour_x=preferences.getInt("toHour",06);
-        toMinute_x=preferences.getInt("toMinute",00);
-        toTimeShow.setText(toHour_x+" : "+toMinute_x);
+        check_mon.setOnCheckedChangeListener(this);
+        check_tue.setOnCheckedChangeListener(this);
+        check_wed.setOnCheckedChangeListener(this);
+        check_thu.setOnCheckedChangeListener(this);
+        check_fri.setOnCheckedChangeListener(this);
+        check_sat.setOnCheckedChangeListener(this);
+        check_sun.setOnCheckedChangeListener(this);
+
+        preferences = getSharedPreferences("MyFiles", MODE_PRIVATE);
+
+        fromHour_x = preferences.getInt("fromHour", 22);
+        fromMinute_x = preferences.getInt("fromMinute", 00);
+        String ftenthHour = String.valueOf(fromHour_x / 10);
+        String fonceHour = String.valueOf(fromHour_x % 10);
+        String ftenthMinute = String.valueOf(fromMinute_x / 10);
+        String fonceMinute = String.valueOf(fromMinute_x % 10);
+        fromTimeShow.setText(ftenthHour + "" + fonceHour + " : " + ftenthMinute + "" + fonceMinute);
+
+        toHour_x = preferences.getInt("toHour", 06);
+        toMinute_x = preferences.getInt("toMinute", 00);
+        String ttenthHour = String.valueOf(toHour_x / 10);
+        String tonceHour = String.valueOf(toHour_x % 10);
+        String ttenthMinute = String.valueOf(toMinute_x / 10);
+        String tonceMinute = String.valueOf(toMinute_x % 10);
+        toTimeShow.setText(ttenthHour + "" + tonceHour + " : " + ttenthMinute + "" + tonceMinute);
+
         showTimePickerDialog();
     }
 
@@ -59,6 +88,7 @@ public class SetTimeActivity extends AppCompatActivity implements CheckBox.OnChe
         );
     }
 
+    @Override
     protected Dialog onCreateDialog(int id) {
         if (id == FROMTEXT_DIALOG_ID)
             return new TimePickerDialog(this, ftTimePickerListener, fromHour_x, fromMinute_x, false);
@@ -71,11 +101,15 @@ public class SetTimeActivity extends AppCompatActivity implements CheckBox.OnChe
         @Override
         public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
             fromHour_x = hourOfDay;
+            String tenthHour = String.valueOf(fromHour_x / 10);
+            String onceHour = String.valueOf(fromHour_x % 10);
             fromMinute_x = minute;
-            fromTimeShow.setText(fromHour_x + " : " + fromMinute_x);
-            SharedPreferences.Editor editor=preferences.edit();
-            editor.putInt("fromHour",fromHour_x);
-            editor.putInt("fromMinute",fromMinute_x);
+            String tenthMinute = String.valueOf(fromMinute_x / 10);
+            String onceMinute = String.valueOf(fromMinute_x % 10);
+            fromTimeShow.setText(tenthHour + "" + onceHour + " : " + tenthMinute + "" + onceMinute);
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putInt("fromHour", fromHour_x);
+            editor.putInt("fromMinute", fromMinute_x);
             editor.commit();
         }
     };
@@ -84,40 +118,69 @@ public class SetTimeActivity extends AppCompatActivity implements CheckBox.OnChe
         @Override
         public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
             toHour_x = hourOfDay;
+            String tenthHour = String.valueOf(toHour_x / 10);
+            String onceHour = String.valueOf(toHour_x % 10);
             toMinute_x = minute;
-            toTimeShow.setText(toHour_x + " : " + toMinute_x);
-            SharedPreferences.Editor editor=preferences.edit();
-            editor.putInt("toHour",toHour_x);
-            editor.putInt("toMinute",toMinute_x);
+            String tenthMinute = String.valueOf(toMinute_x / 10);
+            String onceMinute = String.valueOf(toMinute_x % 10);
+            toTimeShow.setText(tenthHour + "" + onceHour + " : " + tenthMinute + "" + onceMinute);
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putInt("toHour", toHour_x);
+            editor.putInt("toMinute", toMinute_x);
             editor.commit();
         }
     };
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        check_all.setChecked(preferences.getBoolean("checkAll", false));
+        check_mon.setChecked(preferences.getBoolean("checkMon", false));
+        check_tue.setChecked(preferences.getBoolean("checkTue", false));
+        check_wed.setChecked(preferences.getBoolean("checkWed", false));
+        check_thu.setChecked(preferences.getBoolean("checkThu", false));
+        check_fri.setChecked(preferences.getBoolean("checkFri", false));
+        check_sat.setChecked(preferences.getBoolean("checkSat", false));
+        check_sun.setChecked(preferences.getBoolean("checkSun", false));
+    }
+
+    @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        check_mon = (CheckBox) findViewById(R.id.day_mon);
-        check_tue = (CheckBox) findViewById(R.id.day_tue);
-        check_wed = (CheckBox) findViewById(R.id.day_wed);
-        check_thu = (CheckBox) findViewById(R.id.day_thu);
-        check_fri = (CheckBox) findViewById(R.id.day_fri);
-        check_sat = (CheckBox) findViewById(R.id.day_sat);
-        check_sun = (CheckBox) findViewById(R.id.day_sun);
-        if (buttonView.isChecked() == true) {
-            check_mon.setChecked(true);
-            check_tue.setChecked(true);
-            check_wed.setChecked(true);
-            check_thu.setChecked(true);
-            check_fri.setChecked(true);
-            check_sat.setChecked(true);
-            check_sun.setChecked(true);
-        } else {
-            check_mon.setChecked(false);
-            check_tue.setChecked(false);
-            check_wed.setChecked(false);
-            check_thu.setChecked(false);
-            check_fri.setChecked(false);
-            check_sat.setChecked(false);
-            check_sun.setChecked(false);
+
+        if (buttonView.getId() == check_all.getId()) {
+            if (check_all.isChecked()) {
+                check_mon.setChecked(true);
+                check_tue.setChecked(true);
+                check_wed.setChecked(true);
+                check_thu.setChecked(true);
+                check_fri.setChecked(true);
+                check_sat.setChecked(true);
+                check_sun.setChecked(true);
+            }
         }
+       if (check_mon.isChecked() && check_tue.isChecked() && check_wed.isChecked() && check_thu.isChecked() && check_fri.isChecked() && check_sat.isChecked() && check_sun.isChecked()) {
+            check_all.setChecked(true);
+        } else {
+            check_all.setChecked(false);
+        }
+
+        SharedPreferences.Editor editor = preferences.edit();
+        Boolean all=check_all.isChecked();
+        Boolean mon=check_mon.isChecked();
+        Boolean tue=check_tue.isChecked();
+        Boolean wed=check_wed.isChecked();
+        Boolean thu=check_thu.isChecked();
+        Boolean fri=check_fri.isChecked();
+        Boolean sat=check_sat.isChecked();
+        Boolean sun=check_sun.isChecked();
+        editor.putBoolean("checkAll", all);
+        editor.putBoolean("checkMon", mon);
+        editor.putBoolean("checkTue", tue);
+        editor.putBoolean("checkWed", wed);
+        editor.putBoolean("checkThu", thu);
+        editor.putBoolean("checkFri", fri);
+        editor.putBoolean("checkSat", sat);
+        editor.putBoolean("checkSun", sun);
+        editor.commit();
     }
 }
